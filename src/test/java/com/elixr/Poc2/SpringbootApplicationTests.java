826e2ace9c43f8dab07372a41bf685e69859bb66
vinodkadam02 <vinod.kadam@theelixrlabs.com>
2022-12-26 15:41:27 +0530

    Initial Commit

---
+++ src/test/java/com/elixr/Poc2/SpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elixr.Poc2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
