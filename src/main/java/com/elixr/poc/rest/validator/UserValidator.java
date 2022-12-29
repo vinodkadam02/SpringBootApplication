b05491d301dd916d52d0ab557c4fb77ac11b312a
ashutosh2406 <119935143+ashutosh2406@users.noreply.github.com>
2022-12-29 18:11:01 +0530

    using StringUtils.isEmpty() for validation

--- src/main/java/com/elixr/poc/rest/validator/UserValidator.java
+++ src/main/java/com/elixr/poc/rest/validator/UserValidator.java
@@ -3,6 +3,7 @@ package com.elixr.poc.rest.validator;
 import com.elixr.poc.constant.ErrorConstants;
 import com.elixr.poc.rest.request.UserRequest;
 import lombok.Builder;
+import org.springframework.util.StringUtils;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -11,13 +12,13 @@ import java.util.List;
 public class UserValidator {
     public List<String> validate(UserRequest newUserRequest) {
         List<String> errorList = new ArrayList<>();
-        if (newUserRequest.getFirstName() == null || newUserRequest.getFirstName().isEmpty()) {
+        if (StringUtils.isEmpty(newUserRequest.getFirstName())) {
             errorList.add(ErrorConstants.FIRST_NAME_MISSING_IN_USER);
         }
-        if (newUserRequest.getUserName() == null || newUserRequest.getUserName().isEmpty()) {
+        if (StringUtils.isEmpty(newUserRequest.getUserName())) {
             errorList.add(ErrorConstants.USER_NAME_MISSING_IN_USER);
         }
-        if (newUserRequest.getLastName() == null || newUserRequest.getLastName().isEmpty()) {
+        if (StringUtils.isEmpty(newUserRequest.getLastName())) {
             errorList.add(ErrorConstants.LAST_NAME_MISSING_IN_USER);
         }
 
