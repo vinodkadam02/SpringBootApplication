da912b264243039ba0c6392e0818374992f71d61
vivekhit2000 <111848315+vivekhit2000@users.noreply.github.com>
2023-01-02 17:18:29 +0530

    Updated the variable in Purchase Service(purchase post API)

--- src/main/java/com/elixr/poc/service/PurchaseService.java
+++ src/main/java/com/elixr/poc/service/PurchaseService.java
@@ -1,5 +1,7 @@
 package com.elixr.poc.service;
+
 import com.elixr.poc.data.Purchase;
+import com.elixr.poc.repository.PurchaseRepository;
 import com.elixr.poc.rest.request.PurchaseRequest;
 import com.elixr.poc.rest.response.PurchaseErrorResponse;
 import com.elixr.poc.rest.response.PurchaseResponse;
@@ -7,43 +9,40 @@ import com.elixr.poc.rest.validator.PurchaseValidator;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
 import java.util.List;
 import java.util.UUID;
 
 /* service class for editing response and save or update new purchase to database  */
 @Service
 public class PurchaseService {
+    private final PurchaseRepository purchaseRepository;
+
+    public PurchaseService(PurchaseRepository purchaseRepository) {
+        this.purchaseRepository = purchaseRepository;
+    }
+
+    private static Purchase createUserObjectFromRequest(PurchaseRequest purchaseRequest) {
+        return Purchase.builder().userName(purchaseRequest.getUserName()).product(purchaseRequest.getProduct()).amount(purchaseRequest.getAmount()).date(purchaseRequest.getDate()).build();
+    }
+
     public ResponseEntity<?> createPurchase(PurchaseRequest newPurchase) {
         PurchaseResponse newPurchaseResponse;
         Purchase newPurchaseFormat = createUserObjectFromRequest(newPurchase);
         PurchaseErrorResponse newPurchaseErrorResponse;
-        PurchaseValidator userValidator = new PurchaseValidator();
-        List<String> errorList = userValidator.validate(newPurchase);
+        PurchaseValidator purchaseValidator = new PurchaseValidator();
+        List<String> errorList = purchaseValidator.validate(newPurchase);
         HttpStatus httpStatusCode = HttpStatus.OK;
-        if (errorList.isEmpty()) {
-            newPurchaseResponse = PurchaseResponse.builder().success(true)
-                    .id(UUID.randomUUID())
-                    .userName(newPurchaseFormat.getUserName())
-                    .product(newPurchaseFormat.getProduct())
-                    .amount(newPurchaseFormat.getAmount())
-                    .date(newPurchaseFormat.getDate())
-                    .build();
-            return new ResponseEntity<>(newPurchaseResponse, httpStatusCode);
-        } else {
+        if (!errorList.isEmpty()) {
             newPurchaseErrorResponse = PurchaseErrorResponse.builder().success(false).errorMsg(errorList).build();
             httpStatusCode = HttpStatus.BAD_REQUEST;
             return new ResponseEntity<>(newPurchaseErrorResponse, httpStatusCode);
         }
+        newPurchaseResponse = PurchaseResponse.builder().success(true).id(UUID.randomUUID()).userName(newPurchaseFormat.getUserName()).product(newPurchaseFormat.getProduct()).amount(newPurchaseFormat.getAmount()).date(newPurchaseFormat.getDate()).build();
+        saveOrUpdate(newPurchaseFormat);
+        return new ResponseEntity<>(newPurchaseResponse, httpStatusCode);
     }
 
-    private Purchase createUserObjectFromRequest(PurchaseRequest purchaseRequest) {
-        return Purchase.builder().
-                userName(purchaseRequest.getUserName()).
-                product(purchaseRequest.getProduct()).
-                amount(purchaseRequest.getAmount()).
-                date(purchaseRequest.getDate()).
-                build();
-    }
 
     /* to do
     calling the repository to persist the purchase object in db that is in scope of another jira INTTNG-202
@@ -54,6 +53,7 @@ public class PurchaseService {
             UUID uuid = UUID.randomUUID();
             purchase.setId(uuid);
         }
+        purchase = this.purchaseRepository.save(purchase);
         return purchase;
 
     }
