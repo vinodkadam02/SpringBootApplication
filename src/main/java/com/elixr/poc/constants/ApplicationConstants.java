e9ed7aad2419572e37df4e9da30902111757c0f1
vinodkadam02 <vinod.kadam@theelixrlabs.com>
2023-01-04 12:50:10 +0530

    Added Private constructor for ApplicationConstants.

--- src/main/java/com/elixr/poc/constants/ApplicationConstants.java
+++ src/main/java/com/elixr/poc/constants/ApplicationConstants.java
@@ -1,7 +1,10 @@
 package com.elixr.poc.constants;
 
 public class ApplicationConstants {
-    public static final String SUCCESSFULLY_DELETED = "Successfully Deleted";
-    public static final String ID_MISMATCH ="The ID is Not Matching";
 
+    private ApplicationConstants() {
+    }
+
+    public static final String SUCCESSFULLY_DELETED = "Successfully Deleted";
+    public static final String ID_MISMATCH = "The ID is Not Matching";
 }
