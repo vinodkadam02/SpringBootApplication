f025a1a0ccc893128404f0c76501a472eb6a215f
vinodkadam02 <vinod.kadam@theelixrlabs.com>
2023-01-04 19:26:25 +0530

    Reformatting

--- src/main/java/com/elixr/poc/exception/NoRecordFoundException.java
+++ src/main/java/com/elixr/poc/exception/NoRecordFoundException.java
@@ -1,5 +1,7 @@
 package com.elixr.poc.exception;
 
+import java.util.UUID;
+
 public class NoRecordFoundException extends Exception {
     public NoRecordFoundException(String message) {
         super(message);
