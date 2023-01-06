b518c90d5c13a28f5be9e81d5cf25ffb078d5910
ashutosh2406 <119935143+ashutosh2406@users.noreply.github.com>
2023-01-06 14:21:00 +0530

    resolved conflicts

--- src/main/java/com/elixr/poc/rest/response/DeleteResponse.java
+++ src/main/java/com/elixr/poc/rest/response/DeleteResponse.java
@@ -1,9 +1,18 @@
 package com.elixr.poc.rest.response;
 
+import com.elixr.poc.data.User;
+import lombok.Builder;
 import lombok.Data;
 
+import java.util.List;
+
 @Data
-public class DeleteResponse {
-    private boolean success;
+public class DeleteResponse extends ErrorResponse{
+
     private String errorMessage;
+    @Builder
+    public DeleteResponse(String errorMessage) {
+        setSuccess(true);
+        this.errorMessage = errorMessage;
+    }
 }
