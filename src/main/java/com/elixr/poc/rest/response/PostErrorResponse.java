0c288c8b2bf150adcc133a360fef234c81f4a96f
vivekhit2000 <111848315+vivekhit2000@users.noreply.github.com>
2023-01-11 15:19:53 +0530

    Updated get purchase by id

--- src/main/java/com/elixr/poc/rest/response/PostErrorResponse.java
+++ src/main/java/com/elixr/poc/rest/response/PostErrorResponse.java
@@ -2,11 +2,12 @@ package com.elixr.poc.rest.response;
 
 import lombok.Builder;
 import lombok.Data;
+import org.springframework.web.ErrorResponse;
 
 import java.util.List;
 
 @Builder
 @Data
-public class PostErrorResponse extends ErrorResponse{
+public class PostErrorResponse extends AppResponse {
    private List<String> errorMessage;
 }
