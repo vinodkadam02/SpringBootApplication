8142934f847726e50a7d6b4da7fcf846a40e310c
ashutosh2406 <119935143+ashutosh2406@users.noreply.github.com>
2023-01-18 11:33:04 +0530

    committed changes

--- src/main/java/com/elixr/poc/data/Purchase.java
+++ src/main/java/com/elixr/poc/data/Purchase.java
@@ -1,6 +1,5 @@
 package com.elixr.poc.data;
 
-import lombok.Builder;
 import lombok.Data;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
