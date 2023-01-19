0476c81e06b4de17a87ebe4fd45f635fe77dbf3b
vinodkadam02 <vinod.kadam@theelixrlabs.com>
2023-01-19 16:30:32 +0530

    correction

--- src/main/java/com/elixr/poc/rest/controller/UserModificationController.java
+++ src/main/java/com/elixr/poc/rest/controller/UserModificationController.java
@@ -7,7 +7,10 @@ import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
